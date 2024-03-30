a135583e1e1a79ba29d5983affd30c21bf64a762
Richard H. <richard@haddads.net.au>
2024-03-31 00:46:59 +1000

    add links to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,42 +1,94 @@
 ﻿"use client";
 
-import { LuBadgeHelp, LuLayout, LuUserCircle, LuUsers2 } from "react-icons/lu";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
+import {
+  LuBadgeHelp,
+  LuLayout,
+  LuMessageSquare,
+  LuSettings,
+  LuUserCircle,
+  LuUsers2,
+} from "react-icons/lu";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import { Navigation } from "swiper/modules";
 
 export default function NavBar() {
   const pathName = usePathname();
 
-  console.log(pathName);
-
   return (
     <nav
-      className={`mx-auto my-5 px-4 py-4 w-full md:max-w-7xl bg-white rounded-xl overflow-hidden`}
+      className={`relative mx-auto my-5 px-8 w-full md:max-w-7xl bg-white rounded-xl shadow-xl shadow-neutral-300 overflow-hidden`}
     >
-      <div className={`flex w-fit`}>
+      <div
+        className={`absolute left-8 top-1/2 -translate-y-1/2 h-6 w-[1px] bg-neutral-300`}
+      ></div>
+      <Swiper
+        className={`w-full h-20`}
+        slidesPerView={1}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        breakpoints={{
+          324: {
+            slidesPerView: 2,
+          },
+          480: {
+            slidesPerView: 3,
+          },
+          520: {
+            slidesPerView: 4,
+          },
+          640: {
+            slidesPerView: 5,
+          },
+          780: {
+            slidesPerView: 6,
+          },
+          900: {
+            slidesPerView: 7,
+          },
+        }}
+        modules={[Navigation]}
+      >
         {NAV_LINKS.map(({ name, path, icon }, index) => (
-          <article
+          <SwiperSlide
             key={`nav-link-${index}`}
-            className={`group relative inline-block w-32 h-10 border-r border-neutral-200 overflow-hidden`}
+            className={`group relative w-32 h-20 overflow-hidden`}
           >
             <Link
               href={path}
-              className={`absolute top-0 group-hover:-top-10 grid place-content-center w-full transition-all duration-300 ease-in-out`}
+              className={`absolute top-0 group-hover:-top-6 py-7 grid place-content-center w-full transition-all duration-300 ease-in-out`}
             >
               <div
-                className={`grid place-content-center h-10 text-neutral-400 opacity-100 group-hover:opacity-0 transition-all duration-150 ease-in-out`}
+                className={`grid place-content-center h-6 ${
+                  pathName === path ? "text-neutral-800" : "text-neutral-400/60"
+                } opacity-100 group-hover:opacity-0 transition-all duration-150 ease-in-out`}
               >
                 {icon}
               </div>
               <p
-                className={`pt-1 grid place-content-center h-10 text-sm text-neutral-500 group-hover:text-neutral-800 opacity-0 group-hover:opacity-100 transition-all duration-150 ease-in-out`}
+                className={`pt-1 grid place-content-center h-6 text-sm text-neutral-500 group-hover:text-neutral-800 opacity-0 group-hover:opacity-100 transition-all duration-150 ease-in-out`}
               >
                 {name}
               </p>
             </Link>
-          </article>
+            <div
+              className={`absolute right-0 top-1/2 -translate-y-1/2 h-6 w-[1px] bg-neutral-300`}
+            ></div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
+      <div
+        className={`cursor-pointer swiper-button-next absolute right-3 top-1/2 -translate-y-1/2 w-2.5 h-2.5 border-r-2 border-b-2 rounded-sm border-neutral-300 hover:border-neutral-800 -rotate-45 transition-all duration-300 ease-in-out`}
+      ></div>
+      <div
+        className={`cursor-pointer swiper-button-prev absolute left-3 top-1/2 -translate-y-1/2 w-2.5 h-2.5 border-b-2 border-l-2 rounded-sm border-neutral-300 hover:border-neutral-800 rotate-45 transition-all duration-300 ease-in-out`}
+      ></div>
     </nav>
   );
 }
@@ -45,21 +97,31 @@ const NAV_LINKS = [
   {
     name: "Main Feed",
     path: "/",
-    icon: <LuLayout size={26} />,
+    icon: <LuLayout size={22} />,
   },
   {
     name: "Communities",
-    path: "/",
-    icon: <LuUsers2 size={26} />,
+    path: "/communities",
+    icon: <LuUsers2 size={22} />,
   },
   {
     name: "Asks",
-    path: "/",
-    icon: <LuBadgeHelp size={26} />,
+    path: "/asks",
+    icon: <LuBadgeHelp size={22} />,
+  },
+  {
+    name: "My Answers",
+    path: "/my-answers",
+    icon: <LuMessageSquare size={22} />,
+  },
+  {
+    name: "Settings",
+    path: "/my-settings",
+    icon: <LuSettings size={22} />,
   },
   {
     name: "Account",
-    path: "/",
-    icon: <LuUserCircle size={26} />,
+    path: "/my-account",
+    icon: <LuUserCircle size={22} />,
   },
 ];
