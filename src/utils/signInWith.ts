02b7c47cda7203739393e06d9bea66adb933b807
Richard H. <richard@haddads.net.au>
2024-08-25 22:33:24 +1000

    added supabase

---
+++ src/utils/signInWith.ts
@@ -0,0 +1,53 @@
+﻿import { Route } from "next";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+
+const supabase = createClient();
+
+async function signInWithDiscord() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "discord",
+    options: {
+      redirectTo: "/auth/callback",
+    },
+  });
+
+  if (error) console.error(`Discord login failed: ${error.message}`);
+  if (data) redirect(data.url as Route);
+}
+
+async function signInWithFacebook() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "facebook",
+    options: {
+      redirectTo: "/auth/callback",
+    },
+  });
+
+  if (error) console.error(`Facebook login failed: ${error.message}`);
+  if (data) redirect(data.url as Route);
+}
+
+async function signInWithGoogle() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: "/auth/callback",
+    },
+  });
+
+  if (error) console.error(`Google login failed: ${error.message}`);
+  if (data) redirect(data.url as Route);
+}
+
+async function signInWithTwitch() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "twitch",
+    options: {
+      redirectTo: "/auth/callback",
+    },
+  });
+
+  if (error) console.error(`Twitch login failed: ${error.message}`);
+  if (data) redirect(data.url as Route);
+}
